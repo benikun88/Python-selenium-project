f3e0ce1f5ac26dd61664c0eb63beb9d002b24d4a
Beni kun <benikun88@gmail.com>
2024-01-22 21:41:54 +0200

    test github actions

--- pages/login_page.py
+++ pages/login_page.py
@@ -16,32 +16,29 @@ class LoginPage(BasePage):
     WRONG_SIGNIN_ERROR = (By.CSS_SELECTOR, ".message-error.error.message")
     MY_ACCOUNT_BTN = (By.CSS_SELECTOR, "div[class='panel header'] li[class='greet welcome']")
 
-    import allure
+    def __init__(self, driver):
+        super().__init__(driver)
 
-    class YourPageObject:
-        def __init__(self, driver):
-            super().__init__(driver)
+    @allure.step("Fill login information - Email: {email}, Password: {password}")
+    def fill_info(self, email, password):
+        # Fill email field
+        self.fill_text(self.EMAIL_FIELD, email)
+        # Fill password field
+        self.fill_text(self.PASSWORD_FIELD, password)
+        # Click the login button
+        self.click(self.CLICK_BTN)
 
-        @allure.step("Fill login information - Email: {email}, Password: {password}")
-        def fill_info(self, email, password):
-            # Fill email field
-            self.fill_text(self.EMAIL_FIELD, email)
-            # Fill password field
-            self.fill_text(self.PASSWORD_FIELD, password)
-            # Click the login button
-            self.click(self.CLICK_BTN)
+    @allure.step("Retrieve email error message")
+    def get_email_error(self):
+        # Return the text of the email error element
+        return self.get_text(self.EMAIL_FIELD_ERROR)
 
-        @allure.step("Retrieve email error message")
-        def get_email_error(self):
-            # Return the text of the email error element
-            return self.get_text(self.EMAIL_FIELD_ERROR)
+    @allure.step("Retrieve password error message")
+    def get_password_error(self):
+        # Return the text of the password error element
+        return self.get_text(self.PASSWORD_FIELD_ERROR)
 
-        @allure.step("Retrieve password error message")
-        def get_password_error(self):
-            # Return the text of the password error element
-            return self.get_text(self.PASSWORD_FIELD_ERROR)
-
-        @allure.step("Retrieve success login message")
-        def get_success_login(self):
-            # Return the text of the my account button (indicating a successful login)
-            return self.get_text(self.MY_ACCOUNT_BTN)
+    @allure.step("Retrieve success login message")
+    def get_success_login(self):
+        # Return the text of the my account button (indicating a successful login)
+        return self.get_text(self.MY_ACCOUNT_BTN)
