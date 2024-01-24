e8dba9c596ca27dbba459ca0d913372b53b13d3b
Beni kun <benikun88@gmail.com>
2024-01-24 12:06:38 +0200

    test github actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,20 +18,18 @@ def setup(request):
     options = Options()
     options.add_experimental_option("detach", True)
     options.add_argument("--headless")
-    options.add_argument('--start-maximized')
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     # options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
-    # driver = webdriver.Chrome()
     driver.maximize_window()
     driver.get("https://magento.softwaretestingboard.com/")
     yield
     driver.quit()
 
 
-# def pytest_exception_interact(report):
-#     if report.failed:
-#         allure.attach(body=driver.get_screenshot_as_png(), name="screenshot",
-#                       attachment_type=allure.attachment_type.PNG)
+def pytest_exception_interact(report):
+    if report.failed:
+        allure.attach(body=driver.get_screenshot_as_png(), name="screenshot",
+                      attachment_type=allure.attachment_type.PNG)
